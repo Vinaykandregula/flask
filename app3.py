27fa77ebb33e698d38f4bf860a847c7df6cc28ff
S .SUJITH KUMAR <vinaykandregula.180@gmail.com>
2023-08-03 19:45:26 +0530

    added styles

--- app3.py
+++ app3.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('webview.html')
+    return render_template('webview2.html')
 
 @app.route("/success")
 def success():
