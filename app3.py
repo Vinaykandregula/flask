eb374bb3ed0e520cc623afa2fb64f59c9f40b4b6
Vinaykandregula <117073276+Vinaykandregula@users.noreply.github.com>
2023-08-03 17:31:26 +0530

    Add files via upload

---
+++ app3.py
@@ -0,0 +1,40 @@
+### integrating HTML with flask web framework
+# HTTP VERBS GET and POST
+
+from flask import Flask,redirect,url_for,render_template,request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template('webview.html')
+
+@app.route("/success")
+def success():
+    return render_template('output.html',result='PASS')
+
+
+@app.route("/fail")
+def fail():
+    return render_template('output.html',result='FAIL')
+    
+
+### resultchecker html page
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score=0
+    if request.method=='POST':
+        Science=float(request.form['Science'])
+        Maths=float(request.form['Maths'])
+        C=float(request.form['C'])
+        total_score=(Science+Maths+C)/3
+    res=""
+    if total_score>=50:
+        res="success"
+    else:
+        res='fail'
+    return redirect(url_for(res,score=total_score))
+
+if __name__=='__main__':
+    app.run(debug=True)
